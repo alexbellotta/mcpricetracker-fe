8f143f55ca3d20a5d2889b4a6e3bec4aa11f90e2
alexbellotta <alessandrobellotta1998@gmail.com>
2025-09-30 22:21:06 +0200

    [FEAT] api call implementation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'mcpricetracker-fe';
+  title = 'bigmacprice-fe';
+  loggedIn: boolean = false;
+  username: string = '';
+  password: string = '';
+  itemName: string = '';
+  itemPrice: number | null = 0;
+
+  constructor(private http: HttpClient){}
+
+  login(){
+    this.http.post(
+      'http://localhost:3000/api/auth/login',
+      {
+        username: this.username,
+        password: this.password
+      }
+    ).subscribe({
+      next: () => this.loggedIn = true,
+      error: () => alert('Invalid credentials!')
+    })
+  }
+
+  add(){
+    this.http.post('http://localhost:3000/api/hamburgers/add', {
+      name: this.itemName,
+      price: this.itemPrice
+    }).subscribe({
+      next: () => {
+        alert('Item added!');
+        this.itemName = '';
+        this.itemPrice = null;
+      },
+      error: err => alert('Error adding item')
+    });
+  }
 }
